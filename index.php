835f5f82795b75053704d2ac7b0e1df7b1bece52
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-07-01 11:29:49 +0800

    index.php has been cleaned up and a favicon image has been added.

---
+++ index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Jekyll v3.8.6">
+  
+  <title>Sarah's Online Therapy</title>
+
+  <!-- Non-contact form CSS Stylesheets -->
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="navbar.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+
+  <!-- Contact form CSS -->
+  <link rel="stylesheet" type="text/css" href="contact-form/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="contact-form/vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="contact-form/vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="contact-form/css/util.css">
+  <link rel="stylesheet" type="text/css" href="contact-form/css/main.css">
+  <link rel="stylesheet" href="contact-form/vendor/bootstrap/css/bootstrap.css">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+  <link rel="manifest" href="favicon_io/site.webmanifest">
+</head>
+
+<div class="book-call-btn-container d-flex justify-content-sm-end justify-content-center p-3">
+  <a class="book-call-btn" href="#contact">Book a Free Call</a>
+</div>
+
+<div class="no-credit-card-container d-flex justify-content-sm-end">
+  <p>No credit card required</p>
+</div>
+
+<div class="d-flex justify-content-center mt-0"> 
+  <img src="img/online_counselling_logo.png"><h3 class="brand-name mt-5">Sarah's Online Therapy</h3>
+</div>
+
+<nav class="navbar navbar-default navbar-static-top d-flex justify-content-center" id="topnavbar" role="navigation">
+  <div class="navbar-header">
+    <ul class="navbar-list">
+      <li class="nav-list-item"><a class="nav-link" href="#contact">TALK TO ME</a></li>
+      <li class="nav-list-item"><a class="nav-link" href="">BLOG</a></li>
+    </ul>
+  </div>
+</nav>
+
+<img src="https://images.pexels.com/photos/4099096/pexels-photo-4099096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="banner d-flex justify-content-center">
+
+<br><br>
+
+<div>
+  <h1 class="mb-1" align="center">Why Online Therapy?</h1>
+
+  <div class="d-flex justify-content-center">
+    <img src="img/curious.png" alt="">
+  </div>
+
+  <div class="about-therapy">
+    <p>Online therapy is a trend that continutes to grow. Especially during these troubling times. Save yourself time, stress, and money getting to and from each therapy session and get the same benefits from the comfort of your own home!</p>
+  </div>
+
+  <br>
+
+  <div class="about-therapy">
+    <p>Learn more about online therapy and it's benefits <a href="https://www.thrivetalk.com/online-therapy/" target="_blank">here</a>.</p>
+  </div>
+
+</div>
+
+<br><br>
+
+<div class="free-call-strip" align="center">
+  <h1>I Can Help <span>You</span></h1>
+  <p><a href="#contact">BOOK A FREE 20 MINUTE CALL NOW</a></p>
+</div>
+
+<br><br>
+
+<div class="why-choose-me">
+  <div class="container">
+
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <div class="text-center">
+          <h1>Why Choose Me?</h1>
+          <br>
+          <h3><i>What separates me from all the other online therapists?</i></h3>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <!-- Row of selling points -->
+    <div class="row d-flex justify-content-center">
+      <div class="card col-xl-3 col-md-3">
+        <div class="text-center">
+          <div><img src="img/certificate.png" alt=""></div>
+          <h3>Certified</h3>
+          <ul>
+            <li>Masters degree in Occupational Therapy</li>
+            <br>
+            <li>Background checked and certicate validated</li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="card col-xl-3 col-md-3">
+        <div class="text-center">
+          <div><img src="img/experienced.png" alt=""></div>
+          <h3 id="experienced">Experienced</h3>
+          <ul>
+            <li>10+ years of experience helping people overcome life's biggest challenges</li>
+            <br>
+            <li>I've helped people with mood, anxiety, personality and eating disorders</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="card col-xl-3 col-md-3">
+        <div class="text-center">
+          <div><img src="img/respectful.png" alt=""></div>
+          <h3>Respectful</h3>
+          <ul>
+            <li>All information from our talks is <i>forever</i> 100% confidential</li>
+            <br>
+            <li>We can talk through an audio or video call. It's completely up to you!</li>
+            <br>
+            <li>I don't charge an absurd amount of money like other therapists!</li>
+          </ul>
+        </div>
+      </div>
+      <!-- End row of selling points -->
+    
+    </div>
+  </div>
+</div>
+
+<br><br>
+
+<div align="center" class="testimonial-container">
+  <h1 class="testimonial-title">Kind Words</h1>
+
+  <!-- Row of testimonials -->
+  <div class="row">
+
+    <div class="testimonial col-md-4 col-sm-4">
+      <p class="testimonial-paragraph">"I was sceptical about talking with Sarah at first because I had never received online therapy before but... WOW! I feel like my struggles with anxiety have been uplifted even more than when they were when I was doing face-to-face therapy. I would recommend her to anyone."</p><br>
+      <p class="reviewer">- BRETT (FROM LEEDS)</p>
+    </div>
+
+    <div class="testimonial col-md-4 col-sm-4">
+      <p class="testimonial-paragraph">"I had no idea I could improve my mental health while sitting on the couch in my pyjamas. Sarah is a very compassionate therapist whose number one objective is to help make you feel that the future is worth looking forward to."</p><br>
+      <p class="reviewer">- NICOLE (FROM BRIGHTON)</p>
+    </div>
+
+    <div class="testimonial col-md-4 col-sm-4">
+      <p class="testimonial-paragraph">"After having the free 20 minute video call with Sarah I was convinced that pursuing her services was worth the money. I've been receiving therapy sessions for over 15 years and never have I felt as mentally healthy as I do now talking to Sarah every week."</p><br>
+      <p class="reviewer">- DAVE  (FROM NEWCASTLE)</p>
+    </div>
+
+  </div><!--End of row of testimonials-->
+</div>
+
+<!-- Email and validation logic for the contact form -->
+<?php
+  if ($_POST["submit"]) {
+    
+      if (!$_POST['name']) {
+        $error="<br />Please enter your name";
+      }
+      if (!$_POST['email']) {
+        $error.="<br />Please enter your email address";
+      }
+
+      if (!$_POST['message']) {
+        $error.="<br />Please enter a message";
+      }
+        
+      if ($_POST['email']!="" AND !filter_var($_POST['email'],
+
+      FILTER_VALIDATE_EMAIL)) {  
+
+        $error.="<br />Please enter a valid email address";
+
+      } 
+
+      if ($error) {
+        $result='<div class="alert alert-danger"><strong>There were error(s) in your form:</strong>'.$error.'</div>';
+      } 
+      else {
+
+      if (mail("hgreeves@protonmail.com", "Message from Your Website Name", 
+
+      "Name: ".$_POST['name']."
+        Email: ".$_POST['email']."
+        Message: ".$_POST['message'])) {
+
+      $result='<div class="alert alert-success"> <strong> Thank you!</strong> I\'ll get back to you shortly.</div>';
+
+      } 
+      else {
+          $result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
+      }
+    }
+  }
+?>
+
+<div class="container-contact3 mt-5">
+  <div class="wrap-contact3">
+    <form id="contact" class="contact3-form validate-form" method="POST">
+      <span class="contact3-form-title">
+        TALK TO ME
+      </span>
+      
+      <h4 class="text-center">Book a...</h4>
+
+      <br><br>
+
+      <!-- Radio buttons -->
+      <div class="wrap-contact3-form-radio">
+        <div class="contact3-form-radio m-r-42 text-center">
+          <input class="input-radio3" id="radio1" type="radio" name="choice" value="book-call" checked="checked">
+          <label class="label-radio3" for="radio1">
+            Free Call
+          </label>
+        </div>
+
+        <div class="contact3-form-radio">
+          <input class="input-radio3" id="radio2" type="radio" name="choice" value="get-quote">
+          <label class="label-radio3" for="radio2">
+            Therapy Session
+          </label>
+        </div>
+      </div>
+      <!-- End of radio buttons -->
+
+      <div class="wrap-input3 validate-input" data-validate="Name is required">
+        <input class="input3" type="text" name="name" placeholder="Your Name" value="">
+        <span class="focus-input3"></span>
+      </div>
+
+      <div class="wrap-input3 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+        <input class="input3" type="text" name="email" placeholder="Your Email" value="">
+        <span class="focus-input3"></span>
+      </div>
+
+      <div class="wrap-input3 input3-select">
+        <div>
+          <select class="selection-2" name="service">
+            <option>Service</option>
+            <option>CBT (£75/hr)</option>
+            <option>General Counselling (£50/hr)</option>
+          </select>
+        </div>
+        <span class="focus-input3"></span>
+      </div>
+
+      <div class="wrap-input3 validate-input" data-validate = "Message is required">
+        <textarea class="input3" name="message" placeholder="Your Message" value="<?php echo $_POST['message']; ?>"></textarea>
+        <span class="focus-input3"></span>
+      </div>
+
+      <div class="container-contact3-form-btn">
+        <button class="contact3-form-btn" type="submit">
+          Send
+        </button>
+      </div>
+
+    </form>
+  </div>
+</div>
+</div>
+
+<!-- Select option for the contact form -->
+<div id="dropDownSelect1"></div>
+
+<footer>
+  <div id="footer" align="center">
+      <a href="#contact">TALK TO ME</a>
+      <a href="">BLOG</a>
+  </div>
+
+  <div class="footer-links" align="center">
+    <a href="" rel="nofollow">Privacy Policy</a> 
+    <p align="center" class="copyright">&copy; Sarah's Online Therapy 2020</p>
+  </div>
+</footer>
+
+<!-- Third party scripts for the contact form -->
+<script src="contact-form/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="contact-form/vendor/select2/select2.min.js"></script>
+<script src="contact-form/js/main.js"></script>
+
+<script>
+$(".selection-2").select2({
+  minimumResultsForSearch: 20,
+  dropdownParent: $('#dropDownSelect1')
+});
+</script>
+
+</html>
