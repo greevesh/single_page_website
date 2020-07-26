a988a287c212ff98098842b53db8ebcf045fdf53
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-07-26 20:18:34 +0800

    Site is ready on desktop. Just need to make it responsive.

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
   <link rel="stylesheet" href="navbar.css">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Courgette&display=swap">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Sacramento&display=swap" rel="stylesheet">
 
   <!-- Contact form CSS -->
   <link rel="stylesheet" type="text/css" href="contact-form/vendor/bootstrap/css/bootstrap.min.css">
@@ -24,23 +25,18 @@
   <link rel="stylesheet" type="text/css" href="contact-form/css/main.css">
   <link rel="stylesheet" href="contact-form/vendor/bootstrap/css/bootstrap.css">
 
+  <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
   <link rel="manifest" href="favicon_io/site.webmanifest">
+
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <div class="d-flex justify-content-center mt-5"> 
+    <img src="img/online_counselling_logo.png"><h3 class="brand-name mt-5">Sarah's Online Therapy</h3>
+  </div>
 </head>
-
-<div class="book-call-btn-container d-flex justify-content-sm-end justify-content-center p-3">
-  <a class="book-call-btn" href="#contact">Book a Free Call</a>
-</div>
-
-<div class="no-credit-card-container d-flex justify-content-sm-end">
-  <p>No credit card required</p>
-</div>
-
-<div class="d-flex justify-content-center mt-0"> 
-  <img src="img/online_counselling_logo.png"><h3 class="brand-name mt-5">Sarah's Online Therapy</h3>
-</div>
 
 <nav class="navbar navbar-default navbar-static-top d-flex justify-content-center" id="topnavbar" role="navigation">
   <div class="navbar-header">
@@ -51,252 +47,220 @@
   </div>
 </nav>
 
-<img src="https://images.pexels.com/photos/4099096/pexels-photo-4099096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="banner d-flex justify-content-center" alt="An online therapy client">
+<img src="https://www.rhclinical.com/wp-content/uploads/2019/09/online-therapy.jpg" class="banner d-flex justify-content-center" alt="An online therapy client">
 
 <br><br>
 
-<div>
-  <h1 class="mb-1" align="center">Why Online Therapy?</h1>
+<body>
 
-  <div class="d-flex justify-content-center">
-    <img src="img/curious.png" alt="Question mark head">
-  </div>
-
-  <div class="about-therapy">
-    <p>Online therapy is a trend that continutes to grow. Especially during these troubling times. Save yourself time, stress, and money getting to and from each therapy session and get the same benefits from the comfort of your own home!</p>
-  </div>
-
-  <br>
-
-  <div class="about-therapy">
-    <p>Learn more about online therapy and it's benefits <a href="https://www.thrivetalk.com/online-therapy/" target="_blank">here</a>.</p>
-  </div>
-
-</div>
-
-<br><br>
-
-<div class="free-call-strip" align="center">
-  <h1>I Can Help <span>You</span></h1>
-  <p><a href="#contact">BOOK A FREE 20 MINUTE CALL NOW</a></p>
-</div>
-
-<br><br>
-
-<div class="why-choose-me">
   <div class="container">
+    <div class="row">
+      <div class="col-md-6 about-therapy" data-aos="fade-right" data-aos-duration="1000">
+        <h1 class="mb-1 d-flex justify-content-start about-therapy-title" align="center">Why Online Therapy?</h1>
+        <p class="mt-4">Online therapy is a trend that continues to grow. Especially, during these troubling times.</p>
+        <p>Save yourself time, stress, and money commuting to and from face-to-face therapy and reap the same benefits from the comfort of your own home!</p>
+        <p>Learn more about online therapy and it's benefits <a href="https://www.thrivetalk.com/online-therapy/">here</a>.</p>
+      </div>
 
-    <div class="row justify-content-center">
-      <div class="col-lg-6">
-        <div class="text-center">
-          <h1>Why Choose Me?</h1>
-          <br>
-          <h3><i>What separates me from all the other online therapists?</i></h3>
-        </div>
+      <div class="col-md-6">
+        <img src="https://www.salesstar.com/hubfs/Campaign_Assets/Campaign_09/Blog%20images/C8-blog-10-Tips-on-how-to-ask-the-hard-questions.jpg" class="why-therapy" alt="A man with glasses thinking" data-aos="fade-left" data-aos-duration="1000">
       </div>
     </div>
+  </div>
 
-    <br>
+  <br><br>
 
-    <!-- Row of selling points -->
-    <div class="row d-flex justify-content-center">
-      <div class="card col-xl-3 col-md-3">
-        <div class="text-center">
-          <div><img src="img/certificate.png" alt="Certificate icon"></div>
-          <h3>Certified</h3>
-          <ul>
-            <li>Masters degree in Occupational Therapy</li>
+  <div class="free-call-strip" align="center">
+    <h1>I Can Help <span>You</span></h1>
+    <p><a href="#contact">BOOK A FREE 20 MINUTE CALL NOW</a></p>
+    <p class="no-credit-card">No credit card required</p>
+  </div>
+
+  <br><br>
+
+  <div class="why-choose-me">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-6" data-aos="fade-right" data-aos-duration="1000">
+          <div>
+            <h1>Why Choose Me? 🤔</h1>
             <br>
-            <li>Background checked and certicate validated</li>
-          </ul>
+            <img src="https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="profile-pic" alt="Professional profile photo">
+          </div>
         </div>
-      </div>
+
+        <div class="about-me col-6" data-aos="fade-left" data-aos-duration="1000">
+          <h1>Because...</h1>
+          <p class="mt-4">I am a licensed and registered therapist that has been helping people overcome anxiety disorders for the past 10 years.</p>
+          <p>Two years ago I decided to take my services online so that I was able to help a much wider range of people. I haven't looked back since!</p>
+        </div>
       
-      <div class="card col-xl-3 col-md-3">
-        <div class="text-center">
-          <div><img src="img/experienced.png" alt="Experienced icon"></div>
-          <h3 id="experienced">Experienced</h3>
-          <ul>
-            <li>10+ years of experience helping people overcome life's biggest challenges</li>
-            <br>
-            <li>I've helped people with mood, anxiety, personality and eating disorders</li>
-          </ul>
-        </div>
       </div>
-
-      <div class="card col-xl-3 col-md-3">
-        <div class="text-center">
-          <div><img src="img/respectful.png" alt="Respectful icon"></div>
-          <h3>Respectful</h3>
-          <ul>
-            <li>All information from our talks is <i>forever</i> 100% confidential</li>
-            <br>
-            <li>We can talk through an audio or video call. It's completely up to you!</li>
-            <br>
-            <li>I don't charge an absurd amount of money like other therapists!</li>
-          </ul>
-        </div>
-      </div>
-      <!-- End row of selling points -->
-    
     </div>
   </div>
-</div>
 
-<br><br>
+  <br><br>
 
-<div align="center" class="testimonial-container">
-  <h1 class="testimonial-title">Kind Words</h1>
+  <div align="center" class="testimonial-container">
+    <h1 class="testimonial-title">Kind Words</h1>
 
-  <!-- Row of testimonials -->
-  <div class="row">
+    <!-- Row of testimonials -->
+    <div class="row">
 
-    <div class="testimonial col-md-4 col-sm-4">
-      <p class="testimonial-paragraph">"I was sceptical about talking with Sarah at first because I had never received online therapy before but... WOW! I feel like my struggles with anxiety have been uplifted even more than when they were when I was doing face-to-face therapy. I would recommend her to anyone."</p><br>
-      <p class="reviewer">- BRETT (FROM LEEDS)</p>
+      <div class="testimonial col-md-4 col-sm-4">
+        <p class="testimonial-paragraph">"I was sceptical about talking with Sarah at first because I had never received online therapy before but... WOW! I feel like my struggles with anxiety have been uplifted even more than when they were when I was doing face-to-face therapy. I would recommend her to anyone."</p><br>
+        <p class="reviewer">- <i>BRETT (FROM LEEDS)</i></p>
+      </div>
+
+      <div class="testimonial col-md-4 col-sm-4">
+        <p class="testimonial-paragraph">"I had no idea I could improve my mental health while sitting on the couch in my pyjamas. Sarah is a very compassionate therapist whose number one objective is to help make you feel that the future is worth looking forward to."</p><br>
+        <p class="reviewer">- <i>NICOLE (FROM BRIGHTON)</i></p>
+      </div>
+
+      <div class="testimonial col-md-4 col-sm-4">
+        <p class="testimonial-paragraph">"After having the free 20 minute video call with Sarah I was convinced that pursuing her services was worth the money. I've been receiving therapy sessions for over 15 years and never have I felt as mentally healthy as I do now talking to Sarah every week."</p><br>
+        <p class="reviewer">- <i>DAVE  (FROM NEWCASTLE)</i></p>
+      </div>
+
     </div>
-
-    <div class="testimonial col-md-4 col-sm-4">
-      <p class="testimonial-paragraph">"I had no idea I could improve my mental health while sitting on the couch in my pyjamas. Sarah is a very compassionate therapist whose number one objective is to help make you feel that the future is worth looking forward to."</p><br>
-      <p class="reviewer">- NICOLE (FROM BRIGHTON)</p>
-    </div>
-
-    <div class="testimonial col-md-4 col-sm-4">
-      <p class="testimonial-paragraph">"After having the free 20 minute video call with Sarah I was convinced that pursuing her services was worth the money. I've been receiving therapy sessions for over 15 years and never have I felt as mentally healthy as I do now talking to Sarah every week."</p><br>
-      <p class="reviewer">- DAVE  (FROM NEWCASTLE)</p>
-    </div>
-
+    <!--End of row of testimonials-->
   </div>
-  <!--End of row of testimonials-->
-</div>
 
-<div class="container-contact3 mt-5">
-  <div class="wrap-contact3">
-    <form id="contact" class="contact3-form" method="POST">
-      <span class="contact3-form-title">
-        TALK TO ME
-      </span>
-      
-      <h4 class="text-center">Book a...</h4>
+  <div class="container-contact3">
+    <div class="wrap-contact3">
+      <form id="contact" class="contact3-form" method="POST">
+        <span class="contact3-form-title">
+          TALK TO ME
+        </span>
+        
+        <h4 class="text-center">Book a...</h4>
 
-      <br><br>
+        <br><br>
 
-      <!-- Radio buttons -->
-      <div class="wrap-contact3-form-radio">
-        <div class="contact3-form-radio m-r-42 text-center">
-          <input class="input-radio3" id="radio1" type="radio" name="choice" value="book-call" checked="checked">
-          <label class="label-radio3" for="radio1">
-            Free Call
-          </label>
+        <!-- Radio buttons -->
+        <div class="wrap-contact3-form-radio">
+          <div class="contact3-form-radio m-r-42 text-center">
+            <input class="input-radio3" id="radio1" type="radio" name="choice" value="book-call" checked="checked">
+            <label class="label-radio3" for="radio1">
+              Free Call
+            </label>
+          </div>
+
+          <div class="contact3-form-radio">
+            <input class="input-radio3" id="radio2" type="radio" name="choice" value="get-quote">
+            <label class="label-radio3" for="radio2">
+              Therapy Session
+            </label>
+          </div>
+        </div>
+        <!-- End of radio buttons -->
+
+        <div class="wrap-input3">
+          <input class="input3" type="text" name="name" placeholder="Your Name" value="<?php echo $_POST['name']; ?>">
+          <span class="focus-input3"></span>
+          <?php
+            $name = htmlspecialchars($_POST['name']);
+            if (isset($_POST['submit']) && empty($name)) {
+              $warning = "<span style='color: red'>Please enter your name</span>";
+              echo $warning;
+            }
+          ?>
         </div>
 
-        <div class="contact3-form-radio">
-          <input class="input-radio3" id="radio2" type="radio" name="choice" value="get-quote">
-          <label class="label-radio3" for="radio2">
-            Therapy Session
-          </label>
+        <div class="wrap-input3">
+          <input class="input3" type="text" name="email" placeholder="Your Email" value="<?php echo $_POST['email']; ?>">
+          <span class="focus-input3"></span>
+          <?php
+            $email = htmlspecialchars($_POST['email']);
+            if (isset($_POST['submit']) && empty($email)) {
+              $warning = "<span style='color: red'>Please enter your email</span>";
+              echo $warning;
+            }
+            elseif (isset($_POST['submit']) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              $warning = "<span style='color: red;'>Please enter a valid email</span>";
+              echo $warning;
+            }
+          ?>
         </div>
-      </div>
-      <!-- End of radio buttons -->
 
-      <div class="wrap-input3">
-        <input class="input3" type="text" name="name" placeholder="Your Name" value="<?php echo $_POST['name']; ?>">
-        <span class="focus-input3"></span>
-        <?php
-          $name = htmlspecialchars($_POST['name']);
-          if (isset($_POST['submit']) && empty($name)) {
-            $warning = "<span style='color: red'>Please enter your name</span>";
-            echo $warning;
-          }
-        ?>
-      </div>
-
-      <div class="wrap-input3">
-        <input class="input3" type="text" name="email" placeholder="Your Email" value="<?php echo $_POST['email']; ?>">
-        <span class="focus-input3"></span>
-        <?php
-          $email = htmlspecialchars($_POST['email']);
-          if (isset($_POST['submit']) && empty($email)) {
-            $warning = "<span style='color: red'>Please enter your email</span>";
-            echo $warning;
-          }
-          elseif (isset($_POST['submit']) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $warning = "<span style='color: red;'>Please enter a valid email</span>";
-            echo $warning;
-          }
-        ?>
-      </div>
-
-      <div class="wrap-input3 input3-select">
-        <div>
-          <select class="selection-2" name="service">
-            <option>Service</option>
-            <option>CBT (£75/hr)</option>
-            <option>General Counselling (£50/hr)</option>
-          </select>
+        <div class="wrap-input3 input3-select">
+          <div>
+            <select class="selection-2" name="service">
+              <option>Service</option>
+              <option>CBT (£75/hr)</option>
+              <option>General Counselling (£50/hr)</option>
+            </select>
+          </div>
+          <span class="focus-input3"></span>
         </div>
-        <span class="focus-input3"></span>
-      </div>
 
-      <div class="wrap-input3">
-        <textarea class="input3" name="message" placeholder="Your Message" value="<?php echo $_POST['message']; ?>"></textarea>
-        <span class="focus-input3"></span>
-        <?php
-          $message = htmlspecialchars($_POST['message']);
-          if (isset($_POST['submit']) && empty($message)) {
-            $warning = "<span style='color: red'>Please enter a message</span>";
-            echo $warning;
-          }
+        <div class="wrap-input3">
+          <textarea class="input3" name="message" placeholder="Your Message" value="<?php echo $_POST['message']; ?>"></textarea>
+          <span class="focus-input3"></span>
+          <?php
+            $message = htmlspecialchars($_POST['message']);
+            if (isset($_POST['submit']) && empty($message)) {
+              $warning = "<span style='color: red'>Please enter a message</span>";
+              echo $warning;
+            }
+          ?>
+        </div>
+
+        <?php 
+          if (isset($_POST['submit']) && !$warning) {
+            $to = 'hgreeves96@gmail.com';
+            $subject = 'Message request from: ' . $name;
+            $body = 'Message Request' . '<br><br>' .
+                    'Name: '  . $name . '<br><br>' .
+                    'Email: ' . $email . '<br><br>' .
+                    'Message: ' . $message;
+
+            // Includes the chosen service in the email
+            if ($_POST['service'] !== 'Service') {
+              $body .= '<br><br>' . $_POST['service'];
+            }
+
+            $headers  = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+            // Optional headers
+            $headers .= 'To: Harri <hgreeves@protonmail.com>';
+            $headers .= 'From: Message Request <$email>';
+
+            if (!mail($to, $subject, $body, $headers)) {
+              echo "<div class='alert alert-danger'>Sorry. Something went wrong :(</div>";
+            }
+            else
+              echo '<div class="alert alert-success">Message sent. I will get back to you within two working days :)</div>';
+          }   
         ?>
-      </div>
 
-      <?php 
-        if (isset($_POST['submit']) && !$warning) {
-          $to = 'hgreeves96@gmail.com';
-          $subject = 'Message request from: ' . $name;
-          $body = 'Message Request' . '<br><br>' .
-                  'Name: '  . $name . '<br><br>' .
-                  'Email: ' . $email . '<br><br>' .
-                  'Message: ' . $message;
+        <div class="container-contact3-form-btn">
+          <button class="contact3-form-btn" type="submit" name="submit">
+            <span>Send</span>
+          </button>
+        </div>
 
-          // Includes the chosen service in the email
-          if ($_POST['service'] !== 'Service') {
-            $body .= '<br><br>' . $_POST['service'];
-          }
-
-          $headers  = 'MIME-Version: 1.0' . "\r\n";
-          $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-
-          // Optional headers
-          $headers .= 'To: Harri <hgreeves@protonmail.com>';
-          $headers .= 'From: Message Request <$email>';
-
-          if (!mail($to, $subject, $body, $headers)) {
-            echo "<div class='alert alert-danger'>Sorry. Something went wrong :(</div>";
-          }
-          else
-            echo '<div class="alert alert-success">Message sent. I will get back to you within two working days :)</div>';
-        }   
-      ?>
-
-      <div class="container-contact3-form-btn">
-        <button class="contact3-form-btn" type="submit" name="submit">
-          Send
-        </button>
-      </div>
-
-    </form>
+      </form>
+    </div>
   </div>
-</div>
-</div>
+  </div>
 
-<!-- Select option for the contact form -->
-<div id="dropDownSelect1"></div>
+  <!-- Select option for the contact form -->
+  <div id="dropDownSelect1"></div>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
 
 <footer>
   <div id="footer" align="center">
-      <a href="#contact">TALK TO ME</a>
-      <a href="">BLOG</a>
+    <a href="#contact">TALK TO ME</a>
+    <a href="">BLOG</a>
+    <a href=""><img src="img/instagram.png" alt=""></a>
   </div>
 
   <div class="footer-links" align="center">
@@ -311,10 +275,10 @@
 <script src="contact-form/js/main.js"></script>
 
 <script>
-$(".selection-2").select2({
-  minimumResultsForSearch: 20,
-  dropdownParent: $('#dropDownSelect1')
-});
+  $(".selection-2").select2({
+    minimumResultsForSearch: 20,
+    dropdownParent: $('#dropDownSelect1')
+  });
 </script>
 
 </html>
